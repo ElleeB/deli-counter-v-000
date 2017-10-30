543c53fceef5c5b2a10f8946e289bc56161d0d13
Laura Trager <laura.trager@gmail.com>
2017-10-30 02:36:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,19 +2,18 @@ def line(katz_deli)
   if katz_deli.size <= 0
     puts "The line is currently empty."
   else
-    names_with_position = [] # empty array
-    katz_deli.each_with_index do |name, index| # -- go over each and pull name and index
-      line_position = index + 1 # -- new variable to account for +1 to index
-      names_with_position << "#{line_position}" << "#{name}" # -- shovel in names and new line position to empty index
+    names_with_position = [] 
+    katz_deli.each_with_index do |name, index|
+      line_position = index + 1
+      names_with_position << "#{line_position}." << "#{name}"
     end
-    new_string = names_with_position.join(". ") # -- create new string from the array above in order to insert the period with space
-    new_array = new_string.split # -- set variable for new array from the string that includes periods and spaces
-    new_array[1] = names_with_position[1] # -- replace "Name." with the name only stored in n_w_p array
-    new_array[3] = names_with_position[3] # -- samem as previous line
-    final_string = new_array.join(" ") # -- converts array to string, adds spaces, and returns the new string!
+    new_string = names_with_position.join(" ")
     puts "The line is currently: #{final_string}"
   end
 end
+
+# -- if more than 3 people in line, the periods come back after the names: issue with lines 12 & 13
+# -- it 
 
 def take_a_number(katz_deli, new_name)
   katz_deli << new_name
