3d8a496e68a2e4e3960f43d9342b243e434373fb
Laura Trager <laura.trager@gmail.com>
2017-10-30 02:36:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   if katz_deli.size <= 0
     puts "The line is currently empty."
   else
-    names_with_position = [] 
+    names_with_position = []
     katz_deli.each_with_index do |name, index|
       line_position = index + 1
       names_with_position << "#{line_position}." << "#{name}"
@@ -11,9 +11,6 @@ def line(katz_deli)
     puts "The line is currently: #{final_string}"
   end
 end
-
-# -- if more than 3 people in line, the periods come back after the names: issue with lines 12 & 13
-# -- it 
 
 def take_a_number(katz_deli, new_name)
   katz_deli << new_name
