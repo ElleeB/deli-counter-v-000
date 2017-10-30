6b8c2b8a026ce1e10483a55b46045a9d471d9b8c
Laura Trager <laura.trager@gmail.com>
2017-10-30 02:19:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,17 +4,17 @@ def line(katz_deli)
   if katz_deli.size <= 0
     puts "The line is currently empty."
   else
-    names_with_position = []
-    katz_deli.each_with_index do |name, index|
-      line_position = index + 1
-      names_with_position << "#{line_position}" << "#{name}"
+    names_with_position = [] # empty array
+    katz_deli.each_with_index do |name, index| # -- go over each and pull name and index
+      line_position = index + 1 # -- new variable to account for +1 to index
+      names_with_position << "#{line_position}" << "#{name}" # -- shovel in names and new line position to empty index
     end
-    new_string = names_with_position.join(". ")
-    new_array = new_string.split
-    new_array[1] = names_with_position[1]
-    new_array[3] = names_with_position[3]
+    new_string = names_with_position.join(". ") # -- create new string from the array above in order to insert the period with space
+    new_array = new_string.split # -- set variable for new array from the string that includes periods and spaces
+    new_array[1] = names_with_position[1] # -- replace "Name." with the name only stored in n_w_p array
+    new_array[3] = names_with_position[3] # -- samem as previous line
+    new_array.join(" ") # -- converts array to string, adds spaces, and returns the new string!
   end
-  new_array.join(" ")
 end
 
 #=>["1", "Laura", "2", "Ryan", "3", "Avery"]
